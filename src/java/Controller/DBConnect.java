e6b73e141647e691f80f5be488e9e335884f06a8
Hai Tran (kubuntu-e6230) <e6230@bcraft.id.vn>
2026-04-14 13:21:26 +0700

    First Commit

---
+++ src/java/Controller/DBConnect.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import java.sql.*;
+
+public class DBConnect {
+
+    public static Connection getConnection(String dbName) {
+        String dbPort = "3306";
+        String dbUsername = "root";
+        String dbPassword = "";
+
+//        String dbName = "QLBanHang";
+
+        String dbUrl = "jdbc:mysql://localhost:" + dbPort + "/" + dbName;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.getLogger(DBConnect.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        }
+        return null;
+    }
+}
